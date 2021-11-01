20dce6785ee54018bccfe9d70b52cc40502f6151
Ish2504 <87877194+Ish2504@users.noreply.github.com>
2021-11-01 19:34:09 +0000

    done1

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 #Calculator
+from replit import clear
+from art import logo
 
 #Add
 def add(n1, n2):
@@ -23,27 +25,33 @@ operations = {
   '/': divide,
   }
 
-num1 = int(input("What's the first number?: "))
+def calculator():
+  print(logo)
+  num1 = float(input("What's the first number?: "))
 
-for key in operations:
-  print(key)
-operation_simbol = input("Pick an operation from the line above: ")
-num2 = int(input("What's the second number?: "))
-calculation_function = operations[operation_simbol]
-first_answer = calculation_function(num1, num2)
+  for key in operations:
+    print(key)
+  operation_simbol = input("Pick an operation from the line above: ")
+  num2 = float(input("What's the second number?: "))
+  calculation_function = operations[operation_simbol]
+  first_answer = calculation_function(num1, num2)
 
-print(f"{num1} {operation_simbol} {num2} = {first_answer}")
+  print(f"{num1} {operation_simbol} {num2} = {first_answer}")
 
-end_calculation = False
-while not end_calculation:
-  user_choice = input(f"Type 'y' to continue calculating with {first_answer}, or type 'n' to exit.: ")
-  if user_choice == 'y':
-    operation_simbol = input("Pick an operation: ")
-    num3 = int(input("What's the next number?: "))
-    calculation_function = operations[operation_simbol]
-    second_answer = calculation_function(calculation_function(num1, num2), num3)
+  end_calculation = False
+  while not end_calculation:
+    if input(f"Type 'y' to continue calculating with {first_answer}, or type 'n' to start new calculation.: ") == 'y':
+      operation_simbol = input("Pick an operation: ")
+      num3 = int(input("What's the next number?: "))
+      calculation_function = operations[operation_simbol]
+      second_answer = calculation_function(first_answer, num3)
 
-    print(f"{first_answer} {operation_simbol} {num3} = {second_answer}")
-    first_answer = second_answer
-  else:
-    end_calculation = True
+      print(f"{first_answer} {operation_simbol} {num3} = {second_answer}")
+      first_answer = second_answer
+    else:
+      end_calculation = True
+      clear()
+      calculator()
+
+
+calculator()
